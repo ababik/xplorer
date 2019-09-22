b834828ca4677b1e222d265e37b73c9d1cc14c46
Alexey Babik <>
2019-09-22 15:45:39 -0700

    Fix reveal in explorer

--- Source/OperationSystems/WindowsOperationSystem.cs
+++ Source/OperationSystems/WindowsOperationSystem.cs
@@ -11,7 +11,21 @@ namespace Xplorer.OperationSystems
 
         public void Reveal()
         {
-            var argument = "/select, \"" + Context.GetActiveLocation() + "\"";
+            var location = Context.GetActiveLocation();
+            var argument = string.Empty;
+
+            if (location != null)
+            {
+                argument = "\"" + location + "\"";
+
+                var entry = Context.Entries[Context.ActiveIndex];
+                
+                if (entry.Type != NavigationEntryType.NavUpControl)
+                {
+                    argument = "/select, \"" + location + "\"";
+                }
+            }
+
             System.Diagnostics.Process.Start("explorer.exe", argument);
         }
     }
